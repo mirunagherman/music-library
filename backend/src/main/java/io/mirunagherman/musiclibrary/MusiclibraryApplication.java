0b25f39160b6bec022fdc2ec13b703afbf93e9cf
Miruna Gherman <mirunagherman@yahoo.com>
2024-07-15 18:48:27 +0300

    Add main class with application entry point

--- backend/src/main/java/io/mirunagherman/musiclibrary/MusiclibraryApplication.java
+++ backend/src/main/java/io/mirunagherman/musiclibrary/MusiclibraryApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MusiclibraryApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MusiclibraryApplication.class, args);
 	}
 
